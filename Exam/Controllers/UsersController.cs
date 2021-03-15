89bbced8ca338bb665ebfe684dc2ca6fb3ccc95c
leminhhoang-102 <59230706+leminhhoang-102@users.noreply.github.com>
2021-03-15 19:51:26 +0700

    dd

---
+++ Exam/Controllers/UsersController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Exam.Data;
+using Exam.Models;
+using Twilio;
+
+namespace Exam.Controllers
+{
+    public class UsersController : Controller
+    {
+        private ExamContext db = new ExamContext();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View(db.Users.ToList());
+        }
+
+        // GET: Users/Details/5
+     
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,user01,passwork,email,phone,0")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Users.Add(user);
+                db.SaveChanges();
+
+
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User user = db.Users.Find(id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,user01,passwork,email,phone,1")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public ActionResult SendCode()
+        {
+            string accountSid = Environment.GetEnvironmentVariable("ACc6e4604e164ab913274f01a9d0eda47b");
+            string authToken = Environment.GetEnvironmentVariable("7433295b630e9eb433fbe8d0dd7a3657");
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Join Earth's mightiest heroes. Like Kevin Bacon.",
+                from: new Twilio.Types.PhoneNumber("+15017122661"),
+                to: new Twilio.Types.PhoneNumber("+15558675310")
+            );
+
+            return Redirect("Edit");
+        }
+
+
+        public ActionResult Enable(String code)
+        {
+
+
+            return Redirect("Index");
+        }
+      
+    }
+}
---
+++ Exam/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public String user01 { get; set; }
+        public String passwork { get; set; }
+        public String email { get; set; }
+        public String phone { get; set; }
+        public Status status { get; set; }
+
+       public enum Status
+        {
+            ACTIVE = 1,
+            DEACTIVE = 0
+        }
+
+    }
+}
